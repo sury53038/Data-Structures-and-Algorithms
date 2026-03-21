db385afecdc723569454e89793efd22a2fced451
surya53038 <surya53038@gmail.com>
2026-03-21 16:43:00 +0530

    Graph implementation with map and list for random element which are non-linear

--- Graphs/graph_implementation.cpp
+++ Graphs/graph_implementation.cpp
@@ -4,10 +4,39 @@
 
 using namespace std;
 
+class Graph{
+    
+    map<int, list<int>>adj;
+    public:
+    void addEdge(int u, int v){
+        adj[u].push_back(v);
+        // adj[v].push_back(u);
+    }
 
-
-
+    void printGraph(){
+        for(auto it : adj){
+            cout<<it.first<<" -> ";
+            for(auto neig : it.second){
+                cout<<neig<<" ";
+            }
+            cout<<endl;
+        }
+        
+    }
+};
 
 int main(){
+
+    Graph G;
+
+    G.addEdge(12,15);
+    G.addEdge(15,60);
+    G.addEdge(60,40);
+    G.addEdge(40,12);
+
+    G.printGraph();
+    
+
     return 0;
 }
+
